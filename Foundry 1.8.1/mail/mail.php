e2b5290e8000d058af0cb486df2ae99a30510cce
masterlaamaster <vlad.isupov2001@gmail.com>
2017-11-23 08:27:29 +0200

    Inventions

--- Foundry 1.8.1/mail/mail.php
+++ Foundry 1.8.1/mail/mail.php
@@ -10,24 +10,24 @@ $emailMethod                = 'smtp'; // REQUIRED value. Options: 'smtp' , 'phpm
 
 // Outgoing Server Settings - replace values on the right of the = sign with your own.
 // These 3 settings are only required if you choose 'smtp' for emailMethod above.
-$outgoingServerAddress      = 'mail.yourdomain.com'; // Consult your hosting provider.
-$outgoingServerPort         = '25';                  // Options: '587' , '25' - Consult your hosting provider.
+$outgoingServerAddress      = 'mail.zzz.com.ua'; // Consult your hosting provider.
+$outgoingServerPort         = '587';                  // Options: '587' , '25' - Consult your hosting provider.
 $outgoingServerSecurity     = 'tls';                 // Options: 'ssl' , 'tls' , null - Consult your hosting provider.
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
 // These 2 settings are only required if you choose 'smtp' for emailMethod above.
-$sendingAccountUsername     = 'email@domain.com';
-$sendingAccountPassword     = 'p@55w0rd';
+$sendingAccountUsername     = 'mail@teslanow.zzz.com.ua';
+$sendingAccountPassword     = 'X5Misupov';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
-$recipientEmail             = 'email@yourdomain.com'; // REQUIRED value.
-$recipientName              = 'John Doe';             // REQUIRED value.
+$recipientEmail             = 'vlad.isupov2001@gmail.com'; // REQUIRED value.
+$recipientName              = 'Vlad';             // REQUIRED value.
 
 // Email details            - Change these to suit your website needs
-$emailSubject               = 'A message from Your Website'; // REQUIRED value. Subject of the email that the recipient will see
-$websiteName                = 'Your Website';                // REQUIRED value. This is used when a name or email is not collected from the website form
+$emailSubject               = 'Повідомлення з вашого сайту'; // REQUIRED value. Subject of the email that the recipient will see
+$websiteName                = 'Tesla';                // REQUIRED value. This is used when a name or email is not collected from the website form
 
-$timeZone					= 'Australia/Melbourne';         // OPTIONAL, but some servers require this to be set. 
+$timeZone					= 'Europe/Kiev';         // OPTIONAL, but some servers require this to be set.
                                                              // See a list of all supported timezones at: http://php.net/manual/en/timezones.php
  /*----------------------------------------------------------------------------*\
 |*  You do not need to edit anything below this line, the rest is automatic.    *|
